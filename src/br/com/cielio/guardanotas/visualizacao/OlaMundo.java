7349385f70f62d501d8dbf809794af7223ac0e73
César <Csar@PCCM.home>
2014-05-24 02:23:58 +0100

    Ciélio, o código que tu fizes-te nesse arquivo é primário de tão ruim.

--- src/br/com/cielio/guardanotas/visualizacao/OlaMundo.java
+++ src/br/com/cielio/guardanotas/visualizacao/OlaMundo.java
@@ -13,6 +13,5 @@ package br.com.cielio.guardanotas.visualizacao;
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá Ciélio");
-        System.out.println("Olá Cesa");
     }
 }
